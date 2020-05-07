5cc42f9fbdd8fa762a6fd07e307600039f671c6e
jsebasizquierdo <juan_seb.izquierdo@uao.edu.co>
2020-05-07 15:05:49 -0500

    segunda subida

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Section from "./components/Section/Section";
+import Footer from "./components/Footer/Footer";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
     <div className="o-uao-multimedia-repository">
-      
+     <Profile />
     </div>
   );
 }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,46 +1,69 @@
 import React, { Component } from 'react';
+import ProfileImage from "../../resources/img/profile-image.jpg";
+import ProfileName from "../Actuators/ProfileName/ProfileName";
+import DescriptionProfile from "../Actuators/ParagraphJ/DescriptionProfile";
+import './Profile.css';
 
 
 
-
-
-class Layout extends Component {
+class Profile extends Component {
 
     render() {
         return (
-            
-            <div className = "o-uao-profile-container">
 
-                <div className = "o-uao-profile-side-profile">
-                    <div className = "o-uoa-profile-side-photo">
+            <div className="o-uao-profile-container">
 
+
+                <div className="o-uao-profile-side-profile">
+                    <div className="o-uoa-profile-side-photo">
+                        <img src={ProfileImage} className="o-profile-pic"></img>
                     </div>
-                    <div className = "o-uao-profile-side-name">
-
+                    <div className="o-uao-profile-side-name">
+                        <ProfileName nameProfile={"Kevin Stiwar Rondón"} careerProfile={"Ingeniero Multimedia"} />
                     </div>
-                    <div className = "o-uao-profile-side-description">
+                    <div className="o-uao-profile-side-description">
+                        <div className="o-uao-profile-networks">
+                            <a href="https://www.facebook.com/universidadautonomadeoccidente" target="_blank">
+                                <div className="uao-facebook"></div>
+                            </a>
+                            <a href="https://twitter.com/lauao" target="_blank">
+                                <div className="uao-twitter"></div>
+                            </a>
+                            <a href="https://www.youtube.com/user/autonomadeocc" target="_blank">
+                           
+                            <div className="uao-youtube"></div>
+                            </a>
+                        </div>
 
-                    </div>
-                    <div className = "o-uao-profile-side-network">
-
+                        <div className="uao-profile-info">
+                        <DescriptionProfile codeProfile = {"2156473"} emailProfile = {"kevin.rondon@uao.edu.co"} numProfile = {"3009871234"}/>
+                        </div>
                     </div>
 
                 </div>
 
-                <div className = "o-uao-profile-big-container">
+                <div className="o-uao-profile-big-container">
 
-                    <div className = "o-uao-big-container-repositorys-featured">
+                    <div className="o-uao-big-container-repositorys-featured">
+                    <p>
+                    AQUÍ IRÍAN LAS CARDS 1
+
+                    </p>
+
 
                     </div>
-                    <div className = "o-uao-big-container-latest-repositorys">
-                        
+                    <div className="o-uao-big-container-latest-repositorys">
+                      <p>
+                      AQUÍ IRÍAN LAS CARDS 2
+                      </p>
+
                     </div>
                 </div>
 
             </div>
-           
+
         );
     }
 }
 
-export default Footer;
+export default Profile;
