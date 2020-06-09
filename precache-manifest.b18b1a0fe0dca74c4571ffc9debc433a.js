fb4be02356762c4f39004d4f2cc368fd4b1fae10
Kevin Stiwar <kevin.rondon@uao.edu.co>
2020-06-09 14:34:09 -0500

    Updates

---
+++ precache-manifest.b18b1a0fe0dca74c4571ffc9debc433a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad8808ab34da338c9326fb70588093c",
+    "url": "/uao-repository/index.html"
+  },
+  {
+    "revision": "237f24d84559ae6944c2",
+    "url": "/uao-repository/static/css/main.546e9124.chunk.css"
+  },
+  {
+    "revision": "3f151711038b87ed3b11",
+    "url": "/uao-repository/static/js/2.4b6f87fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uao-repository/static/js/2.4b6f87fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "237f24d84559ae6944c2",
+    "url": "/uao-repository/static/js/main.ac60bbcd.chunk.js"
+  },
+  {
+    "revision": "3a84ffe5ffe13ec3b47d",
+    "url": "/uao-repository/static/js/runtime-main.fadc6290.js"
+  },
+  {
+    "revision": "4978eca0d86206ff577c5889b5fa7170",
+    "url": "/uao-repository/static/media/Readme.4978eca0.txt"
+  },
+  {
+    "revision": "b69dda3f8fdd574dbc85eb1cee073a10",
+    "url": "/uao-repository/static/media/about.b69dda3f.jpg"
+  },
+  {
+    "revision": "caa5c020f45b1e74244bcb6262dd33d2",
+    "url": "/uao-repository/static/media/add-repository.caa5c020.png"
+  },
+  {
+    "revision": "89743e163eadc08a604ed3601693c982",
+    "url": "/uao-repository/static/media/home.89743e16.png"
+  },
+  {
+    "revision": "3f7dfbe542fbdebeb2a27ad8a9642aea",
+    "url": "/uao-repository/static/media/logo-repository-login.3f7dfbe5.png"
+  },
+  {
+    "revision": "a3c2ab867538181ace50d6fed66ed290",
+    "url": "/uao-repository/static/media/logouao.a3c2ab86.png"
+  },
+  {
+    "revision": "154807f78ef90cb7900aa481cb9683e0",
+    "url": "/uao-repository/static/media/notification.154807f7.png"
+  },
+  {
+    "revision": "6a33e3584a04af078d996c6c38221f00",
+    "url": "/uao-repository/static/media/profile-image.6a33e358.jpg"
+  }
+]);
